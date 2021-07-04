90ad0656f0247cb243bdfb7a8ec8d19fee62fe52
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2021-07-04 21:07:54 +0700

    import font & icons, change styles

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="<?= base_url('/'); ?>">Navbar</a>
+        <a class="navbar-brand" href="<?= base_url('/'); ?>">Fit Website</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -8,6 +8,20 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Font -->
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap">
+    </noscript>
+
+    <!-- Icon -->
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined&display=swap">
+    </noscript>
+
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="/styles/style.css">
     <title><?= $title; ?></title>
