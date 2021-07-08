31916dd236a9bb406365428af1b7ad341d56fa2e
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2021-07-08 17:01:28 +0700

    moving modal block code

--- app/Views/books/details.php
+++ app/Views/books/details.php
@@ -41,39 +41,39 @@
                                     <i class="material-icons">delete</i>
                                     <span>Delete</span>
                                 </button>
-
-                                <!-- Confirm Book Deletion Modal -->
-                                <div class="modal fade" id="confirmBookDeletion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="staticBackdropLabel">Delete This Book?</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                Are you sure to delete this book?
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-
-                                                <form action="<?= base_url("/books/$book[id]"); ?>" method="post">
-                                                    <?= csrf_field(); ?>
-
-                                                    <!-- Using HTTP Method Spoofing -->
-                                                    <input type="hidden" name="_method" value="DELETE">
-
-                                                    <button type="submit" class="btn btn-danger">
-                                                        <span>Yes, Delete it</span>
-                                                    </button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Confirm Book Deletion Modal -->
+<div class="modal fade" id="confirmBookDeletion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Delete This Book?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Are you sure to delete this book?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+
+                <form action="<?= base_url("/books/$book[id]"); ?>" method="post">
+                    <?= csrf_field(); ?>
+
+                    <!-- Using HTTP Method Spoofing -->
+                    <input type="hidden" name="_method" value="DELETE">
+
+                    <button type="submit" class="btn btn-danger">
+                        <span>Yes, Delete it</span>
+                    </button>
+                </form>
             </div>
         </div>
     </div>
