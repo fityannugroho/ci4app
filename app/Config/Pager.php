71a8d18ceb8c301e6f4589ab44138f4507a8d7fc
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2021-07-08 20:53:22 +0700

    fix naming folder on case-sensitive system env

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -24,7 +24,7 @@ class Pager extends BaseConfig
 		'default_full'   => 'CodeIgniter\Pager\Views\default_full',
 		'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
 		'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-		'books_pagination' => 'App\Views\Pagers\books'
+		'books_pagination' => 'App\Views\pagination\books'
 	];
 
 	/**
